a3afc19d04db56e55615b24c8d34764570038034
Katon Surya Atmaja <katonatmaja@gmail.com>
2023-09-17 00:35:02 +0700

    exercise 3.12
    update .gitignore

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://katlearningbox:${password}@cluster0.5wtejad.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+  const person = new Person({
+    name,
+    number
+  })
+
+  person.save().then(result => {
+    console.log(`added ${name} ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  console.log('phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+}
