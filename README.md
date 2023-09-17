d208627457b296112e50537e3c80d789ade2efa7
Katon Surya Atmaja <katonatmaja@gmail.com>
2023-09-17 14:43:45 +0700

    exercises 3.13-3.18

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ This is repository for my submission of Part 3 exercises on [Full stack open](ht
 Link deployed app:
 https://silent-dream-9258.fly.dev/
 
-The front-end code of this application is [here](https://github.com/katonsa/fullstackopen/tree/exercises3.9-3.11/part2/phonebook)
+The front-end code of this application is [here](https://github.com/katonsa/fullstackopen/tree/for-part3-exercises/part2/phonebook)
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
+
 const app = express()
 
 app.use(express.json())
@@ -14,43 +17,20 @@ morgan.token('request-body', function (req, res) {
 const morganMiddleware = morgan(':method :url :status :res[content-length] - :response-time ms :request-body')
 app.use(morganMiddleware)
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
-app.get('/info', (_request, response) => {
+app.get('/info', (_request, response, next) => {
   const date = new Date()
-  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+  Person.find().then(people => {
+    response.send(`<p>Phonebook has info for ${people.length} people</p><p>${date}</p>`)
+  }).catch(next)
 })
 
 app.get('/api/persons', (_request, response) => {
-  response.json(persons)
+  Person.find().then(people => {
+    response.json(people)
+  })
 })
 
-const generateRandomId = () => {
-  return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-}
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
@@ -65,45 +45,77 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  const isNameExists = persons.find(person => person.name === body.name)
-  if (isNameExists !== undefined) {
-    // Exercise 3.6 I am so confused to use between 400 and 422
-    // As my understanding, 400 is used when its a client error (e.g. malformat request)
-    // but the server can understand the request body so I choose 422
-    // because in MDN website state the server understand the request
-    // but unable to process the instruction
-    return response.status(422).json({
-      error: 'name must be unique'
-    })
-  }
-
-  const person = {
-    id: generateRandomId(),
+  const person = new Person({
     name: body.name,
     number: body.number
+  })
+
+  Person.find({ name: body.name })
+    .then(people => {
+      if (people.length > 0) {
+        return response.status(422).json({
+          error: 'name must be unique'
+        })
+      } else {
+        person.save().then(savedPerson => {
+          response.json(savedPerson)
+        }).catch(next)
+      }
+    }).catch(next)
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    }).catch(next)
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
   }
 
-  persons = persons.concat(person)
-  response.json(person)
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(result => {
+      if (result === null) {
+        response.status(404).end()
+      } else {
+        response.json(result)
+      }
+    }).catch(next)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      if (result === null) {
+        response.status(404).end()
+      } else {
+        response.status(204).end()
+      }
+    })
+    .catch(next)
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-  response.status(204).end()
-})
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
